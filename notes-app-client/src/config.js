f8309c2c7cecdcd98ede3221c952af9c3f185ba0
César García <ultertechnologies@Cesar-Garcia.local>
2019-06-28 09:13:52 -0500

    Login

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,17 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-app-uploads-64"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://vzyhr29iqh.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_R7Rnmplod",
+      APP_CLIENT_ID: "2rmnt9n22145bm9027a6k9atgo",
+      IDENTITY_POOL_ID: "us-east-2:83b1ff39-075b-4a28-8ddc-ee37552734fa"
+    }
+  };
+  
